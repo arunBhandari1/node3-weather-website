736f4080854ce65651d0de72afd3b049df4242ff
Arun <arunbhandari7251@gmail.com>
2021-02-09 14:38:47 -0500

    problem

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const { bgHsl, hsv } = require('chalk')
+
 const geocode =require("./utils/geocode.js")
 const forecast =require("./utils/forecast.js")
 
